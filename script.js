674722e1c6f01c2feacd3a7a79556c1606b89a33
Saaheel Majethia <saaheelm@gmail.com>
2021-03-30 16:14:38 +0530

    Added functionality

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
-console.log("testing");
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totatImages = 0;
+let photosArray = [];
+let initialLoad  = true;
+
+// unsplash API
+let count = 5;
+const apiKey = 'GVQMwtkv86_UircL2liXKk0VThU9IgA2uMG22HxgxV0'; 
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// check if all images are loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if(imagesLoaded === totalImages){
+        ready = true;
+        loader.hidden = true;
+        initialLoad = false;
+        count = 30;
+        apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+    }
+}
+
+// Helper function to set arrtibutes on DOM element
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
+// Create Elements for the Links & Photos, Add to DOM
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Create <a> link to unsplash
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+        // Create <img> for photo
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.urls.description,
+            title: photo.urls.description,
+        });
+        // Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
+        // Put <img> inside <a>, then put both inside imageContainer Element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+// Get Photos from Unsplash 
+async function getPhotos() {
+    try{
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();        
+    } catch(error){
+        // catch error here
+    }
+}
+
+// Check to see if scrolling near bottom of page, load more photos
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight-1000 && ready) {
+        getPhotos();
+        ready = false;
+    }
+})
+
+// on Load
+getPhotos();
